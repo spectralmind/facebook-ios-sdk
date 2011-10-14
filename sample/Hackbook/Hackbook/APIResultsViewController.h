1bb9e30c61ef1c9bd14c16bd768125dbe370f88d
Fabian <ehrentraud@spectralmind.com>
2011-10-14 12:51:29 +0200

    updated library to version from Oct 13, 2011

--- sample/Hackbook/Hackbook/APIResultsViewController.h
+++ sample/Hackbook/Hackbook/APIResultsViewController.h
@@ -6,7 +6,7 @@
  * You may obtain a copy of the License at
  *
  *    http://www.apache.org/licenses/LICENSE-2.0
-
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,38 +14,25 @@
  * limitations under the License.
  */
 
-
 #import <UIKit/UIKit.h>
 #import "FBConnect.h"
-#import "FBLoginButton.h"
 
-
-@interface DemoAppViewController : UIViewController
+@interface APIResultsViewController : UIViewController 
 <FBRequestDelegate,
-FBDialogDelegate,
-FBSessionDelegate>{
-  IBOutlet UILabel* _label;
-  IBOutlet FBLoginButton* _fbButton;
-  IBOutlet UIButton* _getUserInfoButton;
-  IBOutlet UIButton* _getPublicInfoButton;
-  IBOutlet UIButton* _publishButton;
-  IBOutlet UIButton* _uploadPhotoButton;
-  Facebook* _facebook;
-  NSArray* _permissions;
+FBSessionDelegate,
+UITableViewDataSource,
+UITableViewDelegate>{
+    NSMutableArray *myData;
+    NSString *myAction;
+    UILabel *messageLabel;
+    UIView *messageView;
 }
 
-@property(nonatomic, retain) UILabel* label;
+@property (nonatomic, retain) NSMutableArray *myData;
+@property (nonatomic, retain) NSString *myAction;
+@property (nonatomic, retain) UILabel *messageLabel;
+@property (nonatomic, retain) UIView *messageView;
 
-@property(readonly) Facebook *facebook;
-
--(IBAction)fbButtonClick:(id)sender;
-
--(IBAction)getUserInfo:(id)sender;
-
--(IBAction)getPublicInfo:(id)sender;
-
--(IBAction)publishStream:(id)sender;
-
--(IBAction)uploadPhoto:(id)sender;
+- (id)initWithTitle:(NSString *) title data:(NSArray *)data action:(NSString *)action;
 
 @end
